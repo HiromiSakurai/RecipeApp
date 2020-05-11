c4e172cf04505584777fcc57d5ce857393553f65
caraquri-hsakurai <h.sakurai+fr@caraquri.com>
2020-05-12 04:06:58 +0900

    Add Entities

---
+++ RecipeAppKit/Entity/Attributes.swift
@@ -0,0 +1,19 @@
+//
+//  Attributes.swift
+//  RecipeAppKit
+//
+//  Created by 櫻井寛海 on 2020/05/12.
+//  Copyright © 2020 HiromiSakurai. All rights reserved.
+//
+
+import Foundation
+
+public struct Attributes: Decodable {
+    public let title: String
+    public let thumbnailSquareURL: URL
+
+    private enum CodingKeys : String, CodingKey {
+        case title
+        case thumbnailSquareURL = "thumbnail-square-url"
+    }
+}
---
+++ RecipeAppKit/Entity/Recipe.swift
@@ -0,0 +1,21 @@
+//
+//  Recipe.swift
+//  RecipeAppKit
+//
+//  Created by 櫻井寛海 on 2020/05/12.
+//  Copyright © 2020 HiromiSakurai. All rights reserved.
+//
+
+import Foundation
+
+public struct Recipe: Decodable {
+    public let ID: String
+    public let type: String
+    public let attributes: Attributes
+
+    private enum CodingKeys : String, CodingKey {
+        case ID = "id"
+        case type
+        case attributes
+    }
+}
