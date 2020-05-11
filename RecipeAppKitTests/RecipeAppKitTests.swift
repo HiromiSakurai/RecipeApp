d19e073260506b91eb5ac3601656e90768b18c59
caraquri-hsakurai <h.sakurai+fr@caraquri.com>
2020-05-12 05:40:09 +0900

    Add test for getting videos sample

--- RecipeAppKitTests/RecipeAppKitTests.swift
+++ RecipeAppKitTests/RecipeAppKitTests.swift
@@ -7,28 +7,28 @@
 //
 
 import XCTest
+import RxSwift
+import Alamofire
 @testable import RecipeAppKit
 
 class RecipeAppKitTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    override func setUpWithError() throws {}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    override func tearDownWithError() throws {}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func testGetVideosSample() throws {
+        let exp = expectation(description: "get video sample")
+        let client = ClientImpl()
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+        _ = client.request(API.getVideosSample())
+            .subscribe(onSuccess: { res in
+                XCTAssertNotNil(res)
+                exp.fulfill()
+            }, onError: { err in
+                print(err)
+            })
 
+        waitForExpectations(timeout: 3)
+    }
 }
