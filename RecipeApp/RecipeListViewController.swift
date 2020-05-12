30adf0bb0374348b825456e51a06be2c7eb2a206
caraquri-hsakurai <h.sakurai+fr@caraquri.com>
2020-05-13 05:00:39 +0900

    Add RecipeListViewController (No Implementation)

---
+++ RecipeApp/RecipeListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeListViewController.swift
+//  RecipeApp
+//
+//  Created by 櫻井寛海 on 2020/05/13.
+//  Copyright © 2020 HiromiSakurai. All rights reserved.
+//
+
+import UIKit
+
+class RecipeListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
