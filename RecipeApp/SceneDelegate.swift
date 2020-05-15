6eaeac06c42a3685309e05c9196040369311bfc1
caraquri-hsakurai <h.sakurai+fr@caraquri.com>
2020-05-16 02:44:28 +0900

    Add navigation controller

--- RecipeApp/SceneDelegate.swift
+++ RecipeApp/SceneDelegate.swift
@@ -17,7 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = RecipeListViewController()
+        let navigationController = UINavigationController(rootViewController: RecipeListViewController())
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 
