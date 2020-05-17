5caf73436d0d90a4eab04e8b8cfec0a4d07bb41d
HiromiSakurai <sakurai465270@gmail.com>
2020-05-18 05:07:22 +0900

    Add new interface in recipe model

--- RecipeAppKit/RecipeModel.swift
+++ RecipeAppKit/RecipeModel.swift
@@ -8,21 +8,40 @@
 
 import Foundation
 import RxSwift
+import RxRelay
 
 public protocol RecipeModel {
-    func fetchRecipeList() -> Single<[Recipe]>
+    func getRecipeList() -> Observable<[Recipe]>
+    func retrieveRecipe(using index: Int) -> Recipe?
 }
 
 public final class RecipeModelImpl: RecipeModel {
     private let client: Client
+    private let recipeListRelay = BehaviorRelay<[Recipe]>(value: [])
+    private let disposeBag = DisposeBag()
 
     public init() {
         self.client = ClientImpl()
-    }
 
-    public func fetchRecipeList() -> Single<[Recipe]> {
         client
             .request(API.getVideosSample())
             .map(\.data)
+            .asObservable()
+            .bind(to: recipeListRelay)
+            .disposed(by: disposeBag)
+    }
+
+    public func getRecipeList() -> Observable<[Recipe]> {
+        recipeListRelay.asObservable()
+    }
+
+    public func retrieveRecipe(using index: Int) -> Recipe? {
+        recipeListRelay.mutableValue[safe: index] ?? nil
+    }
+}
+
+public extension Collection {
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
     }
 }
