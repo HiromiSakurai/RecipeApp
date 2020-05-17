255264b6644e49a2eab9f010fdbbcc190e237c61
HiromiSakurai <sakurai465270@gmail.com>
2020-05-17 23:55:21 +0900

    Bind view & model for recipe

--- RecipeApp/RecipeListViewController.swift
+++ RecipeApp/RecipeListViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
+import RxDataSources
 
 final class RecipeListViewController: UIViewController {
+    private let viewModel: RecipeListViewModel
+    private let disposeBag = DisposeBag()
+
     private let recipeListCollectionView: UICollectionView = {
         let inset = RecipeListFlowLayout.LayoutConst.contentInset
         let sectionInset = UIEdgeInsets(top: inset, left: inset, bottom: 0, right: inset)
@@ -19,32 +25,51 @@ final class RecipeListViewController: UIViewController {
         return cv
     }()
 
+    private let dataSource = RxCollectionViewSectionedReloadDataSource<SectionOfRecipeCellViewData>(
+        configureCell: { dataSource, collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? RecipeCell else {
+                return UICollectionViewCell()
+            }
+            cell.set(title: item.title, thumbnailURL: item.thumbnailURL, isFavorite: item.isFavorite)
+            return cell
+    })
+
+    init(viewModel: RecipeListViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setUpViews()
+        setUpBindings()
     }
 
     private func setUpViews() {
         navigationItem.title = "料理一覧"
-
-        recipeListCollectionView.dataSource = self
 
         view.addSubview(recipeListCollectionView)
         recipeListCollectionView.snp.makeConstraints { maker in
             maker.edges.equalToSuperview()
         }
     }
-}
 
-// MARK: - UICollectionViewDataSource
-
-extension RecipeListViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        30
+    private func setUpBindings() {
+        viewModel.getRecipeCellViewData()
+            .drive(recipeListCollectionView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
     }
 
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! RecipeCell
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Initializable
+
+extension RecipeListViewController: Initializable {
+    static func initialize(viewModel: RecipeListViewModel) -> RecipeListViewController {
+        .init(viewModel: viewModel)
     }
 }
